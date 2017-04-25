24aa6f4f29ea9cd51e59276ee52149958f825f22
hobosoccer <piotrknott@interia.eu>
2017-04-26 00:02:57 +0200

    Dodanie relacji pomiędzy modelami administratorów, ankiet, pytań oraz odpowiedzi

--- app/models/administrator.rb
+++ app/models/administrator.rb
@@ -1,4 +1,5 @@
 class Administrator < ApplicationRecord
+	has_many :surveys, class_name:Survey, dependent: :destroy, foreign_key: :id
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,5 @@
 class Survey < ApplicationRecord
   has_one :category
+  has_many :questions, class_name:Question, dependent: :destroy
+  belongs_to :administrator, class_name: Administrator, foreign_key: :id
 end
