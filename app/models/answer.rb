26ca137b4fef2b0836961ef122b3a0bf6f2ae813
hobosoccer <piotrknott@interia.eu>
2017-05-25 09:20:15 +0200

    Zmiany w modelach ułatwiające pracę przy tworzeniu wykresów

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < ApplicationRecord
+	has_and_belongs_to_many :respondents, :join_table => :answer_respondents
 	belongs_to :question, class_name:Question, optional: true, :foreign_key => "id"
 	validates :reply, presence: true
 	validates :order, presence: true
--- app/models/respondent.rb
+++ app/models/respondent.rb
@@ -1,4 +1,5 @@
 class Respondent < ApplicationRecord
+	has_and_belongs_to_many :answers, :join_table => :answer_respondents
 	validates :age, numericality: { only_integer: true, greater_than: 0, less_than: 100 }
 	validates :ip_address, presence: true
 end
